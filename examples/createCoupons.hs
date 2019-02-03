2998a39a9283457855cd66ca08144acec7ec5ab7
Chris Martin <ch.martin@gmail.com>
2019-02-03 16:38:00 -0700

    overload strings in example

--- examples/createCoupons.hs
+++ examples/createCoupons.hs
@@ -1,24 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Leanpub.Concepts
 import Leanpub.Wreq
 
-import qualified Data.Text
-import qualified Data.Text.IO
+import Data.Text.IO (appendFile)
+import Prelude hiding (appendFile)
 
 -- Read the API key from a file
 config = configKeyFile "/home/chris/.config/typeclasses/leanpub-api-key.txt"
 
 -- Which book we're creating coupons for
-slug = BookSlug (Data.Text.pack "finding-success-in-haskell")
+slug = BookSlug "finding-success-in-haskell"
 
 -- A note reminding us why the coupon was issued
-note = CouponNote (Data.Text.pack "Free for Type Classes subscriber")
+note = CouponNote "Free for Type Classes subscriber"
 
 -- Each coupon can be used at most twice
 uses = CouponMaxUses 2
 
 -- After creating each coupon, append it to a file
-save (CouponCode x) = Data.Text.IO.appendFile
-  "/home/chris/typeclasses/coupons.txt" (x <> Data.Text.pack "\n")
+save (CouponCode x) =
+    appendFile "/home/chris/typeclasses/coupons.txt" (x <> "\n")
 
 main = runLeanpub config
     (createManyFreeBookCoupons save 200 slug uses (Just note))
